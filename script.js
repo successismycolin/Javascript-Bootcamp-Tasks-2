004e712ec070d840bf6b15c39dbb16a1bb75be72
successismycolin <spumelelebuthelezi@gmail.com>
2026-05-31 21:09:33 +0200

    Working on displaying student result

--- script.js
+++ script.js
@@ -27,5 +27,36 @@ function addStudent() {
     students.push(student);
 }
 
+function displayResult(mark) {
+    result.value = "";
+    mark = Number(mark);
+
+    const studentResult = document.createElement("p");
+
+    switch (true) {
+        case (mark <= 49): 
+            studentResult.innerHTML = "Failed";
+            result.appendChild(studentResult);
+            break;
+        case (mark <= 64):
+            studentResult.innerHTML = "Passed";
+            result.appendChild(studentResult);
+            break;
+        case (mark <= 74):
+            studentResult.innerHTML = "Passed with Merit";
+            result.appendChild(studentResult);
+            break;
+        case (mark <= 100):
+            studentResult.innerHTML = "Passed with Distinction";
+            result.appendChild(studentResult);
+            break;
+        default:
+            studentResult.innerHTML("");
+            result.appendChild(studentResult);
+            break;
+    }
+}
+
+submitBtn.addEventListener("click", displayResult("80"));
 
 // function displayStudent() {}
