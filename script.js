db8e9c06c0b28f2697fa0390805a73f9cae17fa6
successismycolin <spumelelebuthelezi@gmail.com>
2026-05-31 21:08:30 +0200

    Add student function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // <input type=text>
-const nameTextbox = document.getElementById("studentNameText");
+const nameInput = document.getElementById("studentNameText");
 
 // <input type=number>
-const markTextbox = document.getElementById("studentMarkText");
+const markInput = document.getElementById("studentMarkText");
 
-// <input type="button"> 
+// <input type="button">
 const submitBtn = document.getElementById("submitBtn");
 
 // <div>
@@ -12,3 +12,20 @@ const result = document.getElementById("studentResultSection");
 
 // <ul>
 const submittedStudents = document.getElementById("studentsList");
+
+let students = [];
+
+function addStudent() {
+    let name = nameInput.value.trim();
+    let mark = markInput.value.trim();
+
+    let student = {
+        name: name,
+        mark: mark
+    };
+
+    students.push(student);
+}
+
+
+// function displayStudent() {}
