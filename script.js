992a80639df80d74670915b92d7a766256d0af02
successismycolin <spumelelebuthelezi@gmail.com>
2026-05-31 18:28:28 +0200

    Javascript DOM Initialization

---
+++ script.js
@@ -0,0 +1,14 @@
+// <input type=text>
+const nameTextbox = document.getElementById("studentNameText");
+
+// <input type=number>
+const markTextbox = document.getElementById("studentMarkText");
+
+// <input type="button"> 
+const submitBtn = document.getElementById("submitBtn");
+
+// <div>
+const result = document.getElementById("studentResultSection");
+
+// <ul>
+const submittedStudents = document.getElementById("studentsList");
